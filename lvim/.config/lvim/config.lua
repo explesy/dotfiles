714651dfa4ac41ce8bbdfe7280392289e192cc02
ubiq <explesy@gmail.com>
2023-11-10 04:12:36 +0100

    lvim fix: windows moving keys

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -1,8 +1,3 @@
---[[
- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
- `lvim` is the global options object
-]]
-
 -- vim options
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
@@ -20,7 +15,6 @@ lvim.format_on_save = {
 
 -- keymappings <https://www.lunarvim.org/docs/configuration/keybindings>
 lvim.leader = "space"
--- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 
 -- Colemak keys
@@ -44,15 +38,17 @@ lvim.keys.normal_mode[';'] = ':'
 -- Undo
 lvim.keys.normal_mode['U'] = '<C-r>'
 
+-- Windows moving
+lvim.keys.normal_mode["<C-n>"] = "<C-W>j"
+lvim.keys.normal_mode["<C-e>"] = "<C-W>k"
+lvim.keys.normal_mode["<C-i>"] = "<C-W>l"
 
 -- Move line up and down in NORMAL and VISUAL modes
 -- Reference: https://vim.fandom.com/wiki/Moving_lines_up_or_down
--- don't work!!!
-lvim.keys.normal_mode['A-n'] = '<CMD>move .+1<CR>'
-lvim.keys.normal_mode['A-e'] = '<CMD>move .-2<CR>'
-lvim.keys.visual_mode['A-n'] = ":move '>+1<CR>gv=gv'"
-lvim.keys.visual_mode['A-e'] = ":move '>-2<CR>gv=gv'"
-
+lvim.keys.normal_mode['<A-n>'] = '<CMD>move .+1<CR>'
+lvim.keys.normal_mode['<A-e>'] = '<CMD>move .-2<CR>'
+lvim.keys.visual_mode['<A-n>'] = ":move '>+1<CR>gv=gv'"
+lvim.keys.visual_mode['<A-e>'] = ":move '>-2<CR>gv=gv'"
 
 -- Moving throudh buffers left and right
 lvim.keys.normal_mode["<S-i>"] = ":BufferLineCycleNext<CR>"
@@ -64,7 +60,7 @@ lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 
 -- -- Change theme settings
 lvim.colorscheme = 'gruvbox-material'
-vim.g.gruvbox_material_background='hard'
+vim.g.gruvbox_material_background = 'hard'
 
 -- After changing plugin config exit and reopen LunarVim, Run :PackerSync
 lvim.builtin.alpha.active = true
@@ -78,66 +74,20 @@ lvim.builtin.treesitter.auto_install = true
 
 -- lvim.builtin.treesitter.ignore_install = { "haskell" }
 
--- -- generic LSP settings <https://www.lunarvim.org/docs/languages#lsp-support>
-
--- --- disable automatic installation of servers
--- lvim.lsp.installer.setup.automatic_installation = false
-
--- ---configure a server manually. IMPORTANT: Requires `:LvimCacheReset` to take effect
--- ---see the full default list `:lua =lvim.lsp.automatic_configuration.skipped_servers`
--- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
--- local opts = {} -- check the lspconfig documentation for a list of all possible options
--- require("lvim.lsp.manager").setup("pyright", opts)
-
--- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. IMPORTANT: Requires `:LvimCacheReset` to take effect
--- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
--- lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
---   return server ~= "emmet_ls"
--- end, lvim.lsp.automatic_configuration.skipped_servers)
-
--- -- you can set a custom on_attach function that will be used for all the language servers
--- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
--- lvim.lsp.on_attach_callback = function(client, bufnr)
---   local function buf_set_option(...)
---     vim.api.nvim_buf_set_option(bufnr, ...)
---   end
---   --Enable completion triggered by <c-x><c-o>
---   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
--- end
-
--- -- linters and formatters <https://www.lunarvim.org/docs/languages#lintingformatting>
--- local formatters = require "lvim.lsp.null-ls.formatters"
--- formatters.setup {
---   { command = "stylua" },
---   {
---     command = "prettier",
---     extra_args = { "--print-with", "100" },
---     filetypes = { "typescript", "typescriptreact" },
---   },
--- }
--- local linters = require "lvim.lsp.null-ls.linters"
--- linters.setup {
---   { command = "flake8", filetypes = { "python" } },
---   {
---     command = "shellcheck",
---     args = { "--severity", "warning" },
---   },
--- }
-
 -- -- Additional Plugins <https://www.lunarvim.org/docs/plugins#user-plugins>
 lvim.plugins = {
-    {
-      -- "folke/trouble.nvim",
-      -- cmd = "TroubleToggle",
-      'sainnhe/gruvbox-material'
-    },
+  {
+    -- "folke/trouble.nvim",
+    -- cmd = "TroubleToggle",
+    'sainnhe/gruvbox-material'
+  },
 }
 
 -- -- Autocommands (`:help autocmd`) <https://neovim.io/doc/user/autocmd.html>
--- vim.api.nvim_create_autocmd("FileType", {
---   pattern = "zsh",
---   callback = function()
---     -- let treesitter use bash highlight for zsh files as well
---     require("nvim-treesitter.highlight").attach(0, "bash")
---   end,
--- })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "zsh",
+  callback = function()
+    -- let treesitter use bash highlight for zsh files as well
+    require("nvim-treesitter.highlight").attach(0, "bash")
+  end,
+})
