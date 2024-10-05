53f8c19084dbf40a7bef0ee9d7f6d7a4a4a9b68e
Ubiq <doc@kallevala.local>
2024-10-05 21:32:24 +0200

    upd: wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,40 +1,75 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
-local mux = wezterm.mux
-local act = wezterm.action
+-- Use the config_builder in newer versions of wezterm
+local config = wezterm.config_builder and wezterm.config_builder() or {}
 
--- For maximizing window on start
-wezterm.on('gui-startup', function()
-  local tab, pane, window = mux.spawn_window({})
-  window:gui_window():maximize()
+-- Startup configuration
+wezterm.on("gui-startup", function(cmd)
+	local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+	window:perform_action(wezterm.action.ToggleFullScreen, pane)
 end)
 
--- This table will hold the configuration.
-local config = {}
+-- Appearance
+config.color_scheme = "Afterglow (Gogh)"
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font_size = 18.0
+config.dpi = 150.0
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
-if wezterm.config_builder then
-  config = wezterm.config_builder()
-end
-
--- This is where you actually apply your config choices
--- config.color_scheme = 'AdventureTime'
--- config.color_scheme = 'Afterglow'
-config.color_scheme = 'Afterglow (Gogh)'
-config.default_prog = { 'fish', '-l' }
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
+
+config.hide_tab_bar_if_only_one_tab = true
+-- config.tab_bar_at_bottom = true
+config.show_tab_index_in_tab_bar = true
+
+config.inactive_pane_hsb = {
+	saturation = 0.9,
+	brightness = 0.3,
+}
+
+-- Behavior
+config.default_prog = { "/opt/homebrew/bin/fish", "-l" }
 config.use_dead_keys = false
--- config.font = wezterm.font '0xProtoNerdFont'
-config.font = wezterm.font 'JetBrainsMono Nerd Font'
+config.window_close_confirmation = "NeverPrompt"
+config.animation_fps = 60
 
---config.window_decorations = "RESIZE"
+-- Key bindings
+config.keys = {
+	{
+		key = "t",
+		mods = "SHIFT|CTRL",
+		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+	},
+	{
+		key = "f",
+		mods = "SHIFT|CTRL",
+		action = wezterm.action.ToggleFullScreen,
+	},
+}
 
--- and finally, return the configuration to wezterm
+-- Custom color for status line
+config.colors = {
+	tab_bar = {
+		background = "#282c34",
+		active_tab = {
+			bg_color = "#61afef",
+			fg_color = "#282c34",
+		},
+		inactive_tab = {
+			bg_color = "#353b45",
+			fg_color = "#abb2bf",
+		},
+	},
+}
+
+-- Custom command palette
+config.command_palette_fg_color = "#abb2bf"
+config.command_palette_bg_color = "#282c34"
+
 return config
